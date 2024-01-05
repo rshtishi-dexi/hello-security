7bbb972c7c179c7b57581946dfaf281a64580430
Rando Shtishi <rando.shtishi@dexi.io>
2024-01-05 18:32:01 +0100

    Changing storing mechanism from in memory to relation database

---
+++ src/main/java/example/hellosecurity/config/DatabaseConfiguration.java
@@ -0,0 +1,40 @@
+package example.hellosecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:scripts/schema-h2.sql").build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailManager(DataSource dataSource) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        return new JdbcUserDetailsManager(dataSource);
+    }
+}
